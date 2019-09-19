e60f4621ba466ef2b368f087723020920fbae7b4
Daniel Lepage <daniel@daniellepage.com>
2019-09-19 11:43:33 -0400

    mp3

--- docs/app.js
+++ docs/app.js
@@ -10,7 +10,7 @@ var app = new Vue({
     var self = this;
     self.scanner = new Instascan.Scanner({ video: document.getElementById('preview'), scanPeriod: 5 });
     self.scanner.addListener('scan', function (content, image) {
-      self.playSound('beep-07.wav');
+      self.playSound('beep-07.mp3');
       self.scans.unshift({ date: +(Date.now()), content: content });
     });
     Instascan.Camera.getCameras().then(function (cameras) {
